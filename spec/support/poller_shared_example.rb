21fa30d32d426e723298fe8fb3360e0054f0ade1
Jon Phenow <j.phenow@gmail.com>
2013-05-14 10:27:10 -0500

    some more testing that includes some null poller specs

--- spec/support/poller_shared_example.rb
+++ spec/support/poller_shared_example.rb
@@ -10,12 +10,51 @@ shared_examples "a poller" do
 
   it { should respond_to :poll }
 
+  describe "nil loops" do
+    describe "break on nil" do
+      before do
+        subject.stub break_if_nil?: true
+        subject.stub sleep_time: 0
+        subject.stub pop_new_message: nil
+      end
+
+      it "can poll" do
+        subject.should_receive(:sleep).exactly(1).times
+        subject.poll
+      end
+    end
+
+    describe "with max fails" do
+      before do
+        subject.stub max_fails: 10
+        subject.stub(:pop_new_message).and_return(*([nil] * 15))
+      end
+
+      it "can poll" do
+        subject.should_receive(:sleep).exactly(10).times
+        subject.poll
+      end
+    end
+
+    describe "with timeout" do
+      before do
+        subject.stub timeout: 0.1
+        subject.stub(:pop_new_message).and_return(*([nil] * 1500))
+      end
+
+      it "can poll" do
+        subject.should_receive(:sleep).at_least(10).times
+        subject.poll
+      end
+    end
+  end
+
   describe "limited loops" do
     describe "with 2 yields, one nil" do
       before do
         subject.stub break_if_nil?: true
         subject.stub sleep_time: 0
-        subject.stub(:choose_looper).and_yield(message).and_yield nil
+        subject.stub(:pop_new_message).and_return(message, nil)
       end
 
       it "can poll" do
@@ -29,7 +68,7 @@ shared_examples "a poller" do
 
     describe "with timeout" do
       before do
-        subject.stub timeout: 0.2
+        subject.stub timeout: 0.1
         subject.stub(:pop_new_message).and_return(message, *([nil] * 1500))
       end
 
