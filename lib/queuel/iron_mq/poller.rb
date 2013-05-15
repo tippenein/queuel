1212e6c26d15e9ded7f9cc6971a428370110bfff
Jon Phenow <j.phenow@gmail.com>
2013-05-15 08:48:28 -0500

    spec iron poller

--- lib/queuel/iron_mq/poller.rb
+++ lib/queuel/iron_mq/poller.rb
@@ -1,11 +1,14 @@
 module Queuel
   module IronMq
     class Poller < Base::Poller
+      # Public: poll
+      private
+
       def built_options
-        options.merge default_args # intentional direction, force defaults
+        options.merge default_options # intentional direction, force defaults
       end
 
-      def default_args
+      def default_options
         { n: 1 }
       end
     end
--- spec/lib/queuel/iron_mq/poller_spec.rb
+++ spec/lib/queuel/iron_mq/poller_spec.rb
@@ -3,6 +3,16 @@ module Queuel
   module IronMq
     describe Poller do
       it_should_behave_like "a poller"
+
+      let(:queue) { double "Queue" }
+      let(:options) { {} }
+      let(:block) { double "Callable" }
+      subject { described_class.new queue, options, block }
+
+      describe "its options" do
+        its(:built_options) { should == { n: 1 } }
+        its(:default_options) { should == { n: 1 } }
+      end
     end
   end
 end
