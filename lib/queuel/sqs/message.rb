4fe1e43b7c2263533d4e5fa7aa43998a97dde0f3
brady.ouren <brady.ouren@sportngin.com>
2014-04-02 16:33:48 -0500

    cleanup, increase uniqueness of keys, poll sqs for maximum size

--- lib/queuel/sqs/message.rb
+++ lib/queuel/sqs/message.rb
@@ -1,24 +1,24 @@
 module Queuel
   module SQS
     class Message < Base::Message
-      # sqs has a limit of 64kb, 40 is arbitrary number to compensate for
-      # space the hashes take up
-      MAX_BYTESIZE=40*1024
       # if message_object exists (not nil), receive the data, otherwise push
       def raw_body
-        # set based on whether pushing or receiving
-        if @raw_body
-          @raw_body
-        else
-          message_object.nil? ? push_message : pull_message
-        end
+        @raw_body ||= message_object.nil? ? push_message : pull_message
+      end
+
+      def generate_key
+        key = [
+          (Time.now.to_f * 10000).to_i,
+          SecureRandom.urlsafe_base64,
+          Thread.current.object_id
+        ].join('-')
+        key
       end
 
       def push_message
-        # encoded body is just the json string
-        if encoded_body.bytesize > MAX_BYTESIZE
-          key = SecureRandom.urlsafe_base64
-          write_to_s3 encoded_body key
+        if encoded_body.bytesize > max_bytesize
+          key = generate_key
+          write_to_s3(encoded_body, key)
           self.body = { 'queuel_s3_object' => key }
         end
         encoded_body
@@ -33,19 +33,18 @@ module Queuel
             message_object.body
           end
         rescue ::JSON::ParserError, TypeError
-          # if message_object.body isn't a json string
           raw_body_with_sns_check
         end
       end
 
-      def self.s3
-        @s3 ||= get_s3
+      def max_bytesize
+        client_klass.maximum_message_size
       end
 
-      def self.get_s3
-        AWS::S3.new(
-          :access_key_id => options[:access_token],
-          :secret_access_key => options[:secret_access_token] )
+      def self.s3
+        @s3 ||= AWS::S3.new(
+                  :access_key_id => options[:access_token],
+                  :secret_access_key => options[:secret_access_token] )
       end
 
       def read_from_s3 key
@@ -53,14 +52,14 @@ module Queuel
         object.read
       end
 
-      # things written to s3 are expired automatically after 60 days
-      # so there is no need to manually garbage collect them.
-      def write_to_s3 (message, key)
+      # this assumes you've set up some rules for file expiration on your
+      # configured bucket.
+      def write_to_s3(message, key)
         begin
           my_bucket = s3.buckets[options[:bucket_name]]
           my_bucket.objects[key].write(message)
         rescue
-          raise BucketDoesNotExistError "Bucket has either expired or does not exist"
+          raise BucketDoesNotExistError, "Bucket has either expired or does not exist"
         end
       end
 
