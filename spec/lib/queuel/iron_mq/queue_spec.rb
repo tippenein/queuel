0e0918ef2acc1b134ba6b662d6388da84b77d2cb
ryanmuller <cognitionmachine@gmail.com>
2014-04-04 15:31:52 -0500

    move peek tests to ironmq

--- spec/lib/queuel/iron_mq/queue_spec.rb
+++ spec/lib/queuel/iron_mq/queue_spec.rb
@@ -2,9 +2,29 @@ require 'spec_helper'
 module Queuel
   module IronMq
     describe Queue do
+      let(:message) { double "Message", body: "uhuh" }
+      let(:client) { double "ClientObject" }
+      let(:name) { "venues queue" }
       let(:queue_object_with_message) { double "QueueObject", get: message, peek: [message] }
       let(:queue_object_with_nil_message) { double "QueueObject", get: nil, peek: nil }
+
+      subject do
+        described_class.new client, name
+      end
+
       it_should_behave_like "a queue"
+
+      describe "peek" do
+        before do
+          not_for_null do
+            client.stub queue: queue_object_with_message
+          end
+        end
+
+        it "should take options and return an array" do
+          subject.peek(option: true).should be_an Array
+        end
+      end
     end
   end
 end
